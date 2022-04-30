03d2631bbd1b8da3825b1eb186c0eba094deacf7
David Penney <davep15@hotmail.com>
2022-04-30 08:49:27 -0400

    Add files via upload
    Task 3

--- postclick-instagram.spec.js
+++ postclick-instagram.spec.js
@@ -10,11 +10,21 @@
   })*/
 
 
-  it('Demo page - should load footer with all elements and navigate to the privacy policy', () => {
+  /*it('Demo page - should load footer with all elements and navigate to the privacy policy', () => {
    cy.visit('https://instapage.com')
    cy.get('.v7-btn-group > .v7-btn-cta').click()
    cy.scrollTo('bottom')
    cy.get('.v7-mt-80').should('be.visible')
    cy.get('[href="https://instapage.com/privacy-policy"]').click({force: true})
    cy.url().should('eq','https://instapage.com/privacy-policy')
+  })*/
+
+
+
+  it('Resources - should return eight search results', () => {
+   cy.visit('https://instapage.com')
+   cy.get(':nth-child(5) > .main-footer-menu > :nth-child(1) > .v7-btn-flat-black > span').click({force: true})
+   cy.url().should('eq','https://instapage.com/resources')
+   cy.get('.v7-search-input').type('Conversion ROAS{Enter}')
+   cy.get('#js-load-more-content').children().should('have.length', 8)
   })
