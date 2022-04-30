46fbd9a1ce0fae18ad316b7eee845eeb328ab202
David Penney <davep15@hotmail.com>
2022-04-30 11:51:15 -0400

    Add files via upload
    Task 4

--- postclick-instagram.spec.js
+++ postclick-instagram.spec.js
@@ -1,30 +1,34 @@
 /// <reference types="cypress" />
-/*it('Login page - should show an error when trying to login with invalid credentials', () => {
-   cy.visit('https://instapage.com')
 
-   cy.get('.md-visible').click({force: true})
-   cy.get('#email').type('Email@domain.com')
-   cy.get('#password').type('Password')
-   cy.get('form').contains('Log In').click()
-   cy.get('.c-alert__text').should('have.text', 'Incorrect Email or Password')
-  })*/
+describe('Postclick-Instagram actions', () => {
+   beforeEach(() => {
+      cy.visit('https://instapage.com')
+   })
 
 
-  /*it('Demo page - should load footer with all elements and navigate to the privacy policy', () => {
-   cy.visit('https://instapage.com')
-   cy.get('.v7-btn-group > .v7-btn-cta').click()
-   cy.scrollTo('bottom')
-   cy.get('.v7-mt-80').should('be.visible')
-   cy.get('[href="https://instapage.com/privacy-policy"]').click({force: true})
-   cy.url().should('eq','https://instapage.com/privacy-policy')
-  })*/
+   it('Login page - should show an error when trying to login with invalid credentials', () => {
+      cy.get('.md-visible').click({ force: true })
+      cy.get('#email').type('Email@domain.com')
+      cy.get('#password').type('Password')
+      cy.get('form').contains('Log In').click()
+      cy.get('.c-alert__text').should('have.text', 'Incorrect Email or Password')
+   })
+
+
+   it('Demo page - should load footer with all elements and navigate to the privacy policy', () => {
+      cy.get('.v7-btn-group > .v7-btn-cta').click()
+      cy.scrollTo('bottom')
+      cy.get('.v7-mt-80').should('be.visible')
+      cy.get('[href="https://instapage.com/privacy-policy"]').click({ force: true })
+      cy.url().should('eq', 'https://instapage.com/privacy-policy')
+   })
 
 
 
-  it('Resources - should return eight search results', () => {
-   cy.visit('https://instapage.com')
-   cy.get(':nth-child(5) > .main-footer-menu > :nth-child(1) > .v7-btn-flat-black > span').click({force: true})
-   cy.url().should('eq','https://instapage.com/resources')
-   cy.get('.v7-search-input').type('Conversion ROAS{Enter}')
-   cy.get('#js-load-more-content').children().should('have.length', 8)
-  })
+   it('Resources - should return eight search results', () => {
+      cy.get(':nth-child(5) > .main-footer-menu > :nth-child(1) > .v7-btn-flat-black > span').click({ force: true })
+      cy.url().should('eq', 'https://instapage.com/resources')
+      cy.get('.v7-search-input').type('Conversion ROAS{Enter}')
+      cy.get('#js-load-more-content').children().should('have.length', 8)
+   })
+})
