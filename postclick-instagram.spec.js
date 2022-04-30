37a6111b8af50970b010b2b5c927e7e44b0e1d6c
David Penney <davep15@hotmail.com>
2022-04-30 07:20:27 -0400

    Add files via upload
    Task 2

--- postclick-instagram.spec.js
+++ postclick-instagram.spec.js
@@ -1,5 +1,5 @@
 /// <reference types="cypress" />
-it('should navigate to instapage site', () => {
+/*it('Login page - should show an error when trying to login with invalid credentials', () => {
    cy.visit('https://instapage.com')
 
    cy.get('.md-visible').click({force: true})
@@ -7,4 +7,14 @@ it('should navigate to instapage site', () => {
    cy.get('#password').type('Password')
    cy.get('form').contains('Log In').click()
    cy.get('.c-alert__text').should('have.text', 'Incorrect Email or Password')
+  })*/
+
+
+  it('Demo page - should load footer with all elements and navigate to the privacy policy', () => {
+   cy.visit('https://instapage.com')
+   cy.get('.v7-btn-group > .v7-btn-cta').click()
+   cy.scrollTo('bottom')
+   cy.get('.v7-mt-80').should('be.visible')
+   cy.get('[href="https://instapage.com/privacy-policy"]').click({force: true})
+   cy.url().should('eq','https://instapage.com/privacy-policy')
   })
